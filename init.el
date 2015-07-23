d3f109fa10f892ad1ac81f4012caab3465780411
Philip Jägenstedt <philipj@opera.com>
2015-07-23 14:16:32 +0200

    Add .emacs from kirk

--- init.el
+++ init.el
@@ -6,118 +6,83 @@
  '(column-number-mode t)
  '(compilation-scroll-output (quote first-error))
  '(compile-command "cd ~/src/chromium/src/out/Debug; crbuild")
- '(fill-column 80)
+ '(custom-enabled-themes (quote (tango-dark)))
  '(global-auto-revert-mode t)
- '(global-visual-line-mode nil)
- '(haskell-mode-hook (quote (turn-on-haskell-indentation)))
- '(helm-projectile-fuzzy-match nil)
+ '(global-font-lock-mode t nil (font-lock))
+ '(ido-mode (quote buffer) nil (ido))
  '(indent-tabs-mode nil)
  '(make-backup-files nil)
- '(minimap-window-location (quote right))
- '(scroll-bar-mode nil)
+ '(mouse-wheel-mode t nil (mwheel))
+ '(scroll-bar-mode (quote right))
  '(sentence-end-double-space nil)
  '(sgml-basic-offset 1)
  '(show-paren-mode t)
  '(tool-bar-mode nil)
  '(use-file-dialog nil)
- '(visible-bell t)
- '(visual-line-fringe-indicators (quote (nil right-curly-arrow)))
- '(which-function-mode t))
+ '(which-function-mode t)
+ '(x-select-enable-clipboard t))
 
-;; http://stackoverflow.com/questions/9663396/how-do-i-make-emacs-recognize-bash-environment-variables-for-compilation
-(let ((path (shell-command-to-string "bash -l -c 'echo -n $PATH'")))
-  (setenv "PATH" path)
-  (setq exec-path
-        (append
-         (split-string-and-unquote path ":")
-         exec-path)))
+(setq load-path (cons "~/.elisp" load-path))
 
-;; http://emacswiki.org/emacs/ELPA
-(require 'package)
-;;(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-(package-initialize)
+(set-face-attribute 'default nil :height 110)
 
-;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Size-and-Position.html
-(when (window-system)
-  (add-hook 'window-setup-hook
-            (lambda ()
-              (set-frame-position (selected-frame) 0 0)
-              (set-frame-size (selected-frame) 1420 847 t))
-            t))
+(global-set-key "\M-g" 'goto-line)
 
-;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Custom-Themes.html
-(add-to-list 'load-path "~/src/color-theme-sanityinc-tomorrow/")
-(add-to-list 'custom-theme-load-path "~/src/color-theme-sanityinc-tomorrow/")
-(load-theme 'sanityinc-tomorrow-night t)
+(setenv "PAGER" "/bin/cat")
 
-;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Fonts.html
-(add-to-list 'default-frame-alist '(font . "Menlo-13"))
-(setq line-spacing 1)
+(require 'find-things-fast)
+(global-set-key '[f1] 'ftf-find-file)
+(global-set-key '[f2] 'ftf-grepsource)
 
-;; http://tuhdo.github.io/helm-intro.html
-(require 'helm)
-(require 'helm-config)
-(global-set-key (kbd "C-c h") 'helm-command-prefix)
-(global-unset-key (kbd "C-x c"))
-(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
-(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
-(define-key helm-map (kbd "C-z")  'helm-select-action)
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "M-y") 'helm-show-kill-ring)
-(global-set-key (kbd "C-x b") 'helm-mini)
-(global-set-key (kbd "C-x C-b") 'helm-mini)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(setq helm-split-window-in-side-p t)
-(helm-mode 1)
+(global-set-key '[f3] 'vc-git-grep)
+(global-set-key "\C-\M-g" 'vc-git-grep)
+(global-set-key "\C-\M-p" 'previous-error)
+(global-set-key "\C-\M-n" 'next-error)
+(global-set-key (kbd "\C-c o") 'ff-find-other-file)
 
-;; http://tuhdo.github.io/helm-projectile.html
-(projectile-global-mode)
-(setq projectile-completion-system 'helm)
-(helm-projectile-on)
-(setq projectile-enable-caching t)
-(global-set-key (kbd "s-o") 'helm-projectile-find-file)
-(global-set-key (kbd "s-O") 'helm-projectile-find-file-in-known-projects)
+(global-set-key '[f5] 'compile)
+(global-set-key '[f7] 'recompile)
 
+(global-set-key "\M-s\M-l" 'sort-lines)
 
-(global-set-key (kbd "s-f") 'helm-swoop)
+(add-hook 'c++-mode-hook 'flyspell-prog-mode)
+(add-hook 'c-mode-hook 'flyspell-prog-mode)
 
-;;(global-set-key (kbd "s-g") 'helm-ag)
-(global-set-key (kbd "s-g") 'helm-git-grep-at-point)
-
-(global-unset-key (kbd "s-q"))
-(global-set-key (kbd "s-g") 'vc-git-grep)
-(global-set-key (kbd "s-p") 'previous-error)
-(global-set-key (kbd "s-n") 'next-error)
-(global-set-key (kbd "s-s") 'magit-status)
-(global-set-key (kbd "C-c o") 'ff-get-other-file)
-
-;; http://www.gnu.org/software/emacs/manual/html_node/emacs/Compilation.html
-(global-set-key (kbd "s-r")
-                (lambda () (interactive)
-                  (if (fboundp 'recompile) (recompile) (compile))))
-
-;; http://www.emacswiki.org/emacs/DeletingWhitespace#toc3
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-
-;; idl-mode is for something completely different from WebIDL
-(add-to-list 'auto-mode-alist '("\\.idl\\'" . c-mode))
+(defun vc-git-annotate-command (file buf &optional rev)
+  (let ((name (file-relative-name file)))
+    (vc-git-command buf 'async nil "blame" "--date=iso" rev "--" name)))
 
 (fset 'yes-or-no-p 'y-or-n-p)
 
-(require 'tramp)
-(add-to-list 'tramp-remote-path "~/bin")
-(add-to-list 'tramp-remote-path "~/src/depot_tools")
+(require 'google-c-style)
+(add-hook 'c-mode-common-hook 'google-set-c-style)
+(c-add-style "WebKit" '("Google"
+                        (c-basic-offset . 4)
+                        (c-offsets-alist . ((innamespace . 0)
+                                            (access-label . -)
+                                            (case-label . 0)
+                                            (member-init-intro . +)
+                                            (topmost-intro . 0)))))
 
-(add-hook 'c-mode-common-hook 'c-guess)
+(dir-locals-set-class-variables
+ 'google-src
+ '((c-mode . ((c-file-style . "Google")))
+   (c++-mode . ((c-file-style . "Google")))))
+(dir-locals-set-class-variables
+ 'webkit-src
+ '((c-mode . ((c-file-style . "WebKit")))
+   (c++-mode . ((c-file-style . "WebKit")))
+   (idl-mode . ((c-file-style . "WebKit")))))
+(dir-locals-set-directory-class "~/src/opera/" 'google-src)
+(dir-locals-set-directory-class "~/src/opera/chromium/src/third_party/WebKit/" 'webkit-src)
+(dir-locals-set-directory-class "~/src/chromium/src/" 'google-src)
+(dir-locals-set-directory-class "~/src/chromium/src/third_party/WebKit/" 'webkit-src)
 
-;; http://stackoverflow.com/questions/8674912/how-to-collapse-whitespaces-in-a-region
-(defun just-one-space-in-region (beg end)
-  "replace all whitespace in the region with single spaces"
-  (interactive "r")
-  (save-excursion
-    (save-restriction
-      (narrow-to-region beg end)
-      (goto-char (point-min))
-      (while (re-search-forward "\\s-+" nil t)
-        (replace-match " ")))))
+(add-hook 'java-mode-hook (lambda ()
+			    (setq c-basic-offset 4)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
