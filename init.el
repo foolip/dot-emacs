09d5957806497fa24cb858ea3306e65272bf1b91
Philip Jägenstedt <philipj@opera.com>
2015-07-23 16:40:26 +0200

    Split parts of init.el into sulu.el

--- init.el
+++ init.el
@@ -12,7 +12,7 @@
  '(helm-projectile-fuzzy-match nil)
  '(indent-tabs-mode nil)
  '(make-backup-files nil)
- '(scroll-bar-mode nil)
+ '(scroll-bar-mode (quote right))
  '(sentence-end-double-space nil)
  '(sgml-basic-offset 1)
  '(show-paren-mode t)
@@ -23,35 +23,15 @@
  '(which-function-mode t)
  '(x-select-enable-clipboard t))
 
-;; http://stackoverflow.com/questions/9663396/how-do-i-make-emacs-recognize-bash-environment-variables-for-compilation
-(let ((path (shell-command-to-string "bash -l -c 'echo -n $PATH'")))
-  (setenv "PATH" path)
-  (setq exec-path
-        (append
-         (split-string-and-unquote path ":")
-         exec-path)))
-
 ;; http://emacswiki.org/emacs/ELPA
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 (package-initialize)
 
-;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Size-and-Position.html
-(when (window-system)
-  (add-hook 'window-setup-hook
-            (lambda ()
-              (set-frame-position (selected-frame) 0 0)
-              (set-frame-size (selected-frame) 1420 847 t))
-            t))
-
 ;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Custom-Themes.html
 (add-to-list 'load-path "~/src/color-theme-sanityinc-tomorrow/")
 (add-to-list 'custom-theme-load-path "~/src/color-theme-sanityinc-tomorrow/")
 (load-theme 'sanityinc-tomorrow-night t)
-
-;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Fonts.html
-(add-to-list 'default-frame-alist '(font . "Menlo-13"))
-(setq line-spacing 1)
 
 ;; http://tuhdo.github.io/helm-intro.html
 (require 'helm)
@@ -74,36 +54,17 @@
 (setq projectile-completion-system 'helm)
 (helm-projectile-on)
 (setq projectile-enable-caching t)
-(global-set-key (kbd "s-o") 'helm-projectile-find-file)
-(global-set-key (kbd "s-O") 'helm-projectile-find-file-in-known-projects)
-(global-set-key (kbd "s-f") 'helm-swoop)
-(global-set-key (kbd "s-g") 'helm-git-grep-at-point)
 
-(global-unset-key (kbd "s-q"))
-(global-set-key (kbd "s-g") 'vc-git-grep)
-(global-set-key (kbd "s-p") 'previous-error)
-(global-set-key (kbd "s-n") 'next-error)
-(global-set-key (kbd "s-s") 'magit-status)
 (global-set-key (kbd "C-c o") 'ff-get-other-file)
-
-;; http://www.gnu.org/software/emacs/manual/html_node/emacs/Compilation.html
-(global-set-key (kbd "s-r")
-                (lambda () (interactive)
-                  (if (fboundp 'recompile) (recompile) (compile))))
 
 ;; http://www.emacswiki.org/emacs/DeletingWhitespace#toc3
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-;; idl-mode is for something completely different from WebIDL
-(add-to-list 'auto-mode-alist '("\\.idl\\'" . c-mode))
-
 (fset 'yes-or-no-p 'y-or-n-p)
 
-(require 'tramp)
-(add-to-list 'tramp-remote-path "~/bin")
-(add-to-list 'tramp-remote-path "~/src/depot_tools")
-
-(add-hook 'c-mode-common-hook 'c-guess)
+(defun vc-git-annotate-command (file buf &optional rev)
+  (let ((name (file-relative-name file)))
+    (vc-git-command buf 'async nil "blame" "--date=iso" rev "--" name)))
 
 ;; load hostname.el, if it exists
 (let ((hostname (car (split-string system-name "\\."))))
---
+++ sulu.el
@@ -0,0 +1,41 @@
+;; http://stackoverflow.com/questions/9663396/how-do-i-make-emacs-recognize-bash-environment-variables-for-compilation
+(let ((path (shell-command-to-string "bash -l -c 'echo -n $PATH'")))
+  (setenv "PATH" path)
+  (setq exec-path
+        (append
+         (split-string-and-unquote path ":")
+         exec-path)))
+
+;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Size-and-Position.html
+(when (window-system)
+  (add-hook 'window-setup-hook
+            (lambda ()
+              (set-frame-position (selected-frame) 0 0)
+              (set-frame-size (selected-frame) 1420 847 t))
+            t))
+
+;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Fonts.html
+(add-to-list 'default-frame-alist '(font . "Menlo-13"))
+(setq line-spacing 1)
+
+;; http://www.gnu.org/software/emacs/manual/html_node/emacs/Compilation.html
+(global-set-key (kbd "s-r")
+                (lambda () (interactive)
+                  (if (fboundp 'recompile) (recompile) (compile))))
+
+(global-set-key (kbd "s-o") 'helm-projectile-find-file)
+(global-set-key (kbd "s-O") 'helm-projectile-find-file-in-known-projects)
+(global-set-key (kbd "s-f") 'helm-swoop)
+(global-set-key (kbd "s-g") 'helm-git-grep-at-point)
+
+(global-unset-key (kbd "s-q"))
+(global-set-key (kbd "s-g") 'vc-git-grep)
+(global-set-key (kbd "s-p") 'previous-error)
+(global-set-key (kbd "s-n") 'next-error)
+(global-set-key (kbd "s-s") 'magit-status)
+
+(require 'tramp)
+(add-to-list 'tramp-remote-path "~/bin")
+(add-to-list 'tramp-remote-path "~/src/depot_tools")
+
+(add-hook 'c-mode-common-hook 'c-guess)
