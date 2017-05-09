c5121a867c5be7ce83e81e2814d62a1070e7b35a
Philip Jägenstedt <philip@foolip.org>
2017-05-09 10:31:29 +0200

    Open Bikeshed files in web-mode

--- init.el
+++ init.el
@@ -96,6 +96,7 @@
 ;; http://web-mode.org/
 (package-install 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.bs\\'" . web-mode))
 (setq web-mode-markup-indent-offset 1)
 (setq web-mode-css-indent-offset 2)
 (setq web-mode-code-indent-offset 2)
